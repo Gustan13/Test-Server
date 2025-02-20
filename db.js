d5e8fadb99ba925b1ff4dc9f8ab972110f5c5b39
Gustavo Isoo Binder <gustan.binder@gmail.com>
2025-02-20 19:17:53 -0300

    dotenv added

--- db.js
+++ db.js
@@ -1,10 +1,12 @@
+const dotenv = require("dotenv").config()
 const Pool = require("pg").Pool;
 const pool = new Pool({
-    user: "postgres",
-    password: "poop",
-    host: "localhost",
-    port: 5432,
-    database: "todo"
+    user: process.env.PSQL_USER,
+    password: process.env.PSQL_PASSWORD,
+    host: process.env.PSQL_HOST,
+    port: process.env.PSQL_PORT,
+    database: process.env.PSQL_DATABASE
 })
+console.log(process.env)
 
 module.exports = pool;
